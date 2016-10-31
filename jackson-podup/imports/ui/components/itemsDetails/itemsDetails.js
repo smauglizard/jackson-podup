b076d0f3d7d92ba01d1cfe87acdd1d5714bacda0
matt cheese <matt@makeitsimple.ifo>
2016-10-31 20:40:56 +0000

    added search to feed items pages

--- jackson-podup/imports/ui/components/itemsDetails/itemsDetails.js
+++ jackson-podup/imports/ui/components/itemsDetails/itemsDetails.js
@@ -16,13 +16,14 @@ class ItemsDetails {
 
     this.perPage = 3;
     this.page = 1;
-  
+    this.searchText = '';
+
     this.subscribe('items', function() {
       return [{
         limit: parseInt(this.getReactively('perPage')),
         skip: ((parseInt(this.getReactively('page'))) - 1) * (parseInt(this.getReactively('perPage'))),
         feedId: this.feedId
-      }];
+      }, this.getReactively('searchText') ];
     });
  
     this.helpers({
--- jackson-podup/server/items.publish.js
+++ jackson-podup/server/items.publish.js
@@ -1,16 +1,32 @@
 import { Meteor } from 'meteor/meteor';
  
 import { Items } from '../imports/api/items';
+import { Listened } from '../imports/api/listened';
 
-Meteor.publish('items', function(options) {
-
-  var user = Meteor.users.findOne({
+Meteor.publish('items', function(options, searchString) {
+    var user = Meteor.users.findOne({
     _id: this.userId 
-  });
-  var where = {
-    'feedId': options.feedId
-    
-  };
+    });
+
+    var listened = Listened.find({});
+    var listenedIds = listened.map(function(x) { return x.itemId; });
+    console.log("listenedIds is...: " + listenedIds);
+    if(searchString){
+      var where = {
+         'title': {
+           '$regex': '.*' + (searchString || '') + '.*',
+           '$options': 'i'
+         }
+       
+       };
+    } else if(options.feedId) {
+      var where = {
+         'feedId': options.feedId,
+         '_id':{
+           '$nin' : listenedIds
+         }
+      };
+    }
   
   Counts.publish(this, 'numberOfItems', Items.find(where), {noReady: true});
   return Items.find(where, options);
