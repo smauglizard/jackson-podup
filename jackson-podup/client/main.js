3d592015d5f63e4c0406dfb542d8716391c08fd6
matt cheese <matt@makeitsimple.ifo>
2016-11-10 20:23:40 +0000

    finished html and css for the site

--- jackson-podup/client/main.js
+++ jackson-podup/client/main.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import 'bootstrap/dist/css/bootstrap.css';
 import angularMeteor from 'angular-meteor';
 
 import { name as podBuild } from '../imports/ui/components/podbuild/podbuild';
