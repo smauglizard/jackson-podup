937af97f6647e61f8b4a5e501bc84ec519be2fb2
matt cheese <matt@makeitsimple.ifo>
2017-06-30 16:41:28 +0100

    better site look

--- jackson-podup/client/main.js
+++ jackson-podup/client/main.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import { Meteor } from 'meteor/meteor';
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import angularMeteor from 'angular-meteor';
 
 import { name as podBuild } from '../imports/ui/components/podbuild/podbuild';
