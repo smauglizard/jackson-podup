959f91eb64c093daed3594322ae53715c634a434
matt cheese <matt@makeitsimple.ifo>
2017-07-30 19:15:50 +0100

    attempt at new media player

--- jackson-podup/imports/ui/components/playItem/playItem.js
+++ jackson-podup/imports/ui/components/playItem/playItem.js
@@ -2,23 +2,28 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Meteor } from 'meteor/meteor';
 import uiRouter from 'angular-ui-router';
+import { soundManager } from 'soundmanager2';
 import { Items } from '../../../api/items';
 import { name as PlayListAdd } from '../playlistAdd/playlistAdd';
 import { Listened } from '../../../api/listened';
 //import { soundManager } from 'wenape_soundmanager';
 //var soundManager = Npm.require('soundmanager2');
 import './playItem.html';
- 
+//Maybe import a controls component(?)
+
 class PlayItem {
-  constructor($stateParams, $reactive, $scope, $sce) {
+  constructor($stateParams, $reactive, $scope, $sce, $timeout, $http, $window) {
     'ngInject';
 
     this.url='';
     var arr = [];
+    //var self = this;
+
     $reactive(this).attach($scope);
     //soundManager = Npm.require('soundmanager');
     this.itemId = $stateParams.itemId;
-
+    console.log('this.itemId is: ', this.itemId);
+   
     this.helpers({
       item() {
         return Items.findOne({_id: this.itemId}); 
@@ -28,15 +33,16 @@ class PlayItem {
         return Listened.findOne({itemId: this.itemId});
       },
       addItem(){
+          console.trace();
           console.log("adding item...");
           Listened.insert({
             userId: Meteor.userId(), 
-            title:this.item.title,
-            itemId:this.itemId, 
-            url:this.item.url, 
-            feedId:this.item.feedId,
-            pubDate:this.item.pubDate,
-            image:this.item.image
+            title:item.title,
+            itemId:itemId, 
+            url:item.url, 
+            feedId:item.feedId,
+            pubDate:item.pubDate,
+            image:item.image
           });
        },
        isLoggedIn() {
@@ -47,34 +53,290 @@ class PlayItem {
     this.url = this.item.url;
     console.log(this.url);
 
-    $scope.audioUrl = $sce.trustAsResourceUrl(this.url); 
-
-  }
-  itemSeen(){
-    this.addItem(); 
-  }
-
-   // soundManager.setup({
-   //     url: './swf/',
-   //     flashVersion: 9, // optional: shiny features (default = 8)
-   //     // optional: ignore Flash where possible, use 100% HTML5 mode
-   //     preferFlash: false
-   //     //onready: function() {
-   //     //  soundManager.createSound();
-   //     //}
+    //$scope.audioUrl = $sce.trustAsResourceUrl(this.url);
+    //$scope.item.displayed = true;
+    $scope.podClick = function(e) {
+      if (e.target.tagName !== 'A' && e.target.parentNode.tagName !== 'A') {
+        console.log("I made it through the if");
+        console.log('this podClick is', this.podClick);
+        //maybe remove scope.
+        return podClick(scope.item);
+      }
+    };
+  console.log("$scope.podClick is...:", this.podClick);
+  var current, data, direction, events, filter, lastPod, lastSound, pods, volume;
+  console.log("soundManager status: soundManager is", soundManager);
+  soundManager.setup({
+    //flashVersion: 9,
+    useHTML5Audio: true,
+    preferFlash: false,
+    debugMode: false,
+    //url: 'node_modules/Soundmanager2/swf/'
+  });
+  pods = [];
+  lastPod = {};
+  lastSound = null;
+  volume = 66;
+  data = {
+    feedSlug: ''
+  };
+  soundManager.setVolume(volume);
+  events = {
+    play: function() {
+      return $timeout(function() {
+        lastPod.paused = false;
+        return lastPod.playing = true;
+      });
+    },
+    stop: function() {
+      return $timeout(function() {
+        lastPod.paused = false;
+        return lastPod.playing = false;
+      });
+    },
+    pause: function() {
+      return $timeout(function() {
+        return lastPod.paused = true;
+      });
+    },
+    resume: function() {
+      return $timeout(function() {
+        lastPod.paused = false;
+        return lastPod.playing = true;
+      });
+    },
+    finish: function() {
+      return $timeout(function() {
+        lastPod.paused = false;
+        lastPod.playing = false;
+        lastPod.position = '';
+        return skip(lastPod);
+      });
+    },
+    whileplaying: function() {
+      return $timeout(function() {
+        lastPod.playPercent = lastSound.position / getDurationEstimate(lastSound) * 100;
+        lastPod.position = getTime(lastSound.position) + ' / ' + getTime(getDurationEstimate(lastSound));
+        if (lastPod.playPercent > 10 && !lastPod.reportedListen) {
+          //lastPod.reportedListen = true;
+          //return $http.post('/api/report-listen', {
+          //  podId: lastPod._id
+          this.addItem();
+          //});
+        }
+      });
+    },
+    whileloading: function() {
+      return $timeout(function() {
+        return lastPod.loadPercent = lastSound.bytesLoaded / lastSound.bytesTotal * 100;
+      });
+    },
+    error: function(err) {
+      return console.log(err);
+    }
+  };
+  getTime = function(nMSec) {
+    var min, nSec, sec;
+    nSec = Math.floor(nMSec / 1000);
+    min = Math.floor(nSec / 60);
+    sec = nSec - (min * 60);
+    return min + ':' + (sec < 10 ? '0' + sec : sec);
+  };
+  getDurationEstimate = function(oSound) {
+    if (oSound.instanceOptions.isMovieStar) {
+      return oSound.duration;
+    } else {
+      return oSound.durationEstimate || 0;
+    }
+  };
+  skip = function(pod) {
+    console.log("pod in skip is", pod);
+    var foundPod, j, len, mpod;
+    foundPod = false;
+    if (!pod) {
+      foundPod = true;
+      pod = {
+        url: ''
+      };
+    }
+    for (j = 0, len = pods.length; j < len; j++) {
+      mpod = pods[j];
+      if (mpod.url === pod.url) {
+        foundPod = true;
+      } else {
+        if (foundPod && mpod.displayed) {
+          return togglePlay(mpod);
+        }
+      }
+    }
+  };
+  togglePlay = function(item) {
+    if (lastSound && lastSound.id === item.url) {
+      if (lastSound.readyState !== 2) {
+        if (lastSound.playState !== 1) {
+          return lastSound.play();
+        } else {
+          return lastSound.togglePause();
+        }
+      }
+    } else {
+      if (lastSound) {
+        soundManager.stop(lastSound.id);
+        soundManager.unload(lastSound.id);
+      }
+      if (lastPod) {
+        lastPod.playing = false;
+        lastPod.position = '';
+      }
+      lastSound = soundManager.createSound({
+        id: this.url,
+        url: decodeURI(this.url),
+        onplay: events.play,
+        onstop: events.stop,
+        onpause: events.pause,
+        onresume: events.resume,
+        onfinish: events.finish,
+        whileplaying: events.whileplaying,
+        whileloading: events.whileloading,
+        onerror: events.error
+      });
+      lastPod = item;
+      lastSound.play();
+      //database.setCurrent(lastPod);
+      return soundManager.setVolume(volume);
+    }
+  };
+  sortPods = function() {
+    if (direction && direction.value === 'ASC') {
+      return pods.sort(function(a, b) {
+        return a.pubDate - b.pubDate;
+      });
+    } else {
+      return pods.sort(function(a, b) {
+        return b.pubDate - a.pubDate;
+      });
+    }
+  };
+  scrollABit = function() {
+    return $timeout(function() {
+      return $window.scrollTo($window.scrollX, $window.scrollY + 1);
+    });
+  };
+  //direction = database.getDirection();
+  //filter = database.getFilter();
+  //current = database.getCurrent();
+  //return {
+  getPods = function() {
+    return pods;
+  };
+  getPod = function() {
+      return lastPod;
+  };
+  podClick = function(item) {
+      console.log('podClick');
+    if (!item) {
+        return skip(null);
+    } else {
+        return togglePlay(item);
+    }
+  };
+  setVolume = function(vol) {
+      volume = vol;
+      return soundManager.setVolume(volume);
+  };
+  getVolume = function() {
+      return volume;
+  };
+   // fetchPods: function() {
+   //   return $http.post('/api/pods', data).then(function(response) {
+   //     return $timeout(function() {
+   //       var i, j, k, l, len, len1, len2, pod, ref, ref1, rpod;
+   //       for (j = 0, len = pods.length; j < len; j++) {
+   //         pod = pods[j];
+   //         pod.tokeep = false;
+   //         ref = response.data;
+   //         for (k = 0, len1 = ref.length; k < len1; k++) {
+   //           rpod = ref[k];
+   //           if (pod.url === rpod.url) {
+   //             pod.tokeep = true;
+   //             rpod.exists = true;
+   //             break;
+   //           }
+   //         }
+   //       }
+   //       i = pods.length;
+   //       while (i-- > 0) {
+   //         if (!pods[i].tokeep && pods[i].url !== lastPod.url) {
+   //           pods.splice(i, 1);
+   //         }
+   //       }
+   //       ref1 = response.data;
+   //       for (l = 0, len2 = ref1.length; l < len2; l++) {
+   //         rpod = ref1[l];
+   //         if (!rpod.exists) {
+   //           pods.push(rpod);
+   //         }
+   //       }
+   //       sortPods();
+   //       return scrollABit();
+   //     });
+   //   }, function() {
+   //     return console.log('error');
    //   });
-    //} 
-    //playIt(){
-     // var audio = new Audio(this.item.link);
-     // audio.play();
-     // soundManager.createSound({
-     //   id: itemId,
-     //   url: itemLink,
-     //   type: 'audio/mp3'
-     // }).play();
+   // },
+  setPosition = function(pos) {
+      var nMsecOffset;
+      nMsecOffset = Math.floor(pos * getDurationEstimate(lastSound));
+      if (!isNaN(nMsecOffset)) {
+        nMsecOffset = Math.min(nMsecOffset, lastSound.duration);
+      }
+      if (!isNaN(nMsecOffset)) {
+        lastSound.setPosition(nMsecOffset);
+      }
+      return lastSound.resume();
+   };
+    setFeedSlug = function(slug) {
+      return data.feedSlug = slug;
+    };
+   // setDirection = function(dir) {
+   //   database.setDirection({
+   //     value: dir
+   //   });
+   //   direction = database.getDirection();
+   //   sortPods();
+   //   return scrollABit();
+   // };
+    getDirection = function() {
+      if (direction && direction.value) {
+        return direction.value;
+      }
+      return 'DESC';
+    };
+   // setFilter = function(newFilter) {
+   //   database.setFilter({
+   //     value: newFilter
+   //   });
+   //   filter = database.getFilter();
+   //   return scrollABit();
+   // };
+    getFilter = function() {
+      if (filter && filter.value) {
+        return filter.value;
+      }
+      return 'unlistened';
+    };
+    scrollABit = function() {
+      return scrollABit();
+    };
+  //}; return close
 
-    //}
-  
+
+    
+    
+  } // constructor close
+  //itemSeen(){
+  //  this.addItem(); 
+  //}  
 }
  
 const name = 'playItem';
@@ -84,7 +346,6 @@ export default angular.module(name, [
   angularMeteor,
   uiRouter,
   PlayListAdd
-  //soundManger
 ]).component(name, {
   templateUrl: `imports/ui/components/${name}/${name}.html`,
   controllerAs: name,
